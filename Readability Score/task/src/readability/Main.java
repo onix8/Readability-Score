661170dfe7730d164220a566ad42084bce950555
ONIX <zachem-nado@mail.ru>
2020-06-14 20:00:01 +0300

    Stage 2/4: Words and sentences (Solution)

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -6,7 +6,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        int length = text.length();
-        System.out.print(length <= 100 ? "EASY" : "HARD");
+        String[] sentences = text.split("[.!?]+");
+        int countSentences = sentences.length;
+        int[] countWords = new int[countSentences];
+        String[][] sentencesSplitAnWords = new String[countSentences][];
+
+        for (int i = 0; i < countSentences; i++) {
+            String sentence = sentences[i];
+            sentencesSplitAnWords[i] = sentence.replaceAll("\\W", " ").split("\\s+");
+            int counterWords = 0;
+            for (int j = 0; j < sentencesSplitAnWords[i].length; j++) {
+                String word = sentencesSplitAnWords[i][j];
+                if (word.matches("^\\w*\\w$")) {
+                    counterWords++;
+                }
+            }
+            countWords[i] = counterWords;
+        }
+
+        int sumCountAllWords = 0;
+
+        for (int count : countWords) {
+            sumCountAllWords += count;
+        }
+
+        double averageCountWordsInSentence = ((double) sumCountAllWords) / ((double) countSentences);
+
+        String readability = averageCountWordsInSentence > 10 ? "HARD" : "EASY";
+        System.out.println(readability);
     }
 }
